cae5d0e56e223eaea382719211f410a4734d049f
minnewanka <philippe.inthavong@gmail.com>
2020-12-30 09:33:13 +0900

    init in18l

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
+import { useTranslation } from "react-i18next";
 import styled from "styled-components";
+import i18n from "./i18n";
+import Home from "./pages/Home";
+import Settings from "./pages/Settings";
 
 const Title = styled.h1`
   font-size: 1.5em;
@@ -7,12 +11,21 @@ const Title = styled.h1`
   color: green;
 `;
 
-function App() {
+const App: React.FC = () => {
+  const [page, setPage] = useState(0);
+  const { t } = useTranslation();
+  const changeLanguage = (lng: string) => {
+    i18n.changeLanguage(lng);
+  };
+
   return (
     <div className="App">
-      <Title>Hello World</Title>
+      <Title>{t("title")}</Title>
+      <button onClick={() => changeLanguage("en")}>EN</button>
+      <button onClick={() => changeLanguage("fr")}>FR</button>
+      {page === 0 ? <Home /> : <Settings />}
     </div>
   );
-}
+};
 
 export default App;
